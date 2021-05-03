230d0351290ebf55773bd161d97f54669e6344e7
qszhu <qszhu2006@gmail.com>
2021-05-03 11:17:38 +0800

    update

--- 107/sketch.js
+++ 107/sketch.js
@@ -8,34 +8,45 @@ const colors = [
 
 let sandpiles;
 let nextpiles;
+const blockSize = 8;
+let rows;
+let cols;
+
+function array2D(rows, cols, fill) {
+  return new Array(rows).fill().map(() => new Array(cols).fill(fill));
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
 
-  sandpiles = new Array(height).fill().map(() => new Array(width).fill(0));
-  nextpiles = new Array(height).fill().map(() => new Array(width).fill(0));
+  rows = Math.trunc(height / blockSize);
+  cols = Math.trunc(width / blockSize);
 
-  sandpiles[Math.trunc(height / 2)][Math.trunc(width / 2)] = 10 ** 9;
+  sandpiles = array2D(rows, cols, 0);
+  nextpiles = array2D(rows, cols, 0);
+
+  sandpiles[Math.trunc(rows / 2)][Math.trunc(cols / 2)] =
+    Number.MAX_SAFE_INTEGER;
 
   background(...defaultColor);
 }
 
 function topple() {
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
       nextpiles[y][x] = sandpiles[y][x];
     }
   }
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
       const num = sandpiles[y][x];
       if (num < 4) continue;
       nextpiles[y][x] -= 4;
-      if (x + 1 < width) nextpiles[y][x + 1]++;
+      if (x + 1 < cols) nextpiles[y][x + 1]++;
       if (x - 1 >= 0) nextpiles[y][x - 1]++;
-      if (y + 1 < height) nextpiles[y + 1][x]++;
+      if (y + 1 < rows) nextpiles[y + 1][x]++;
       if (y - 1 >= 0) nextpiles[y - 1][x]++;
     }
   }
@@ -46,22 +57,21 @@ function topple() {
 }
 
 function render() {
-  loadPixels();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  noStroke();
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
       const num = sandpiles[y][x];
       const col = num < 4 ? colors[num] : defaultColor;
-      const idx = (x + y * width) * 4;
-      for (let i = 0; i < 3; i++) {
-        pixels[idx + i] = col[i];
-      }
+      fill(...col);
+      rect(x * blockSize, y * blockSize, blockSize, blockSize);
     }
   }
-  updatePixels();
 }
 
 function draw() {
   render();
 
-  topple();
+  for (let i = 0; i < 1; i++) {
+    topple();
+  }
 }
